aa6a9e3b01d97f526906479b50e6b3966c3c391d
LorenaReina <lorenareina2016@gmail.com>
2025-04-02 16:50:02 -0500

    Refactor project structure from Ant to Maven

---
+++ src/main/java/service/UsuarioApiService.java
@@ -0,0 +1,47 @@
+
+package main.java.service;
+
+import java.util.List;
+import main.java.modelo.Usuario;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ *
+ * @author lreinac
+ */
+public interface UsuarioApiService {
+    @GET("/api/usuarios")
+    Call<List<Usuario>> getAllUsuarios();
+
+    @GET("/api/usuarios/{id}")
+    Call<Usuario> getUsuarioById(@Path("id") String id);
+
+    @POST("/api/usuarios")
+    Call<Usuario> createUsuario(@Body Usuario usuario);
+
+    @PUT("/api/usuarios/{id}")
+    Call<Usuario> updateUsuario(@Path("id") String id, @Body Usuario usuario);
+
+    @DELETE("/api/usuarios/{id}")
+    Call<Void> deleteUsuario(@Path("id") String id);
+
+    @GET("/api/usuarios/buscar")
+    Call<List<Usuario>> buscarUsuarios(
+        @Query("nombre") String nombre,
+        @Query("email") String email,
+        @Query("edad") int edad
+    );
+
+    @GET("/api/usuarios/auth")
+    Call<Usuario> getUserByToken(@Header("Authorization") String authToken);
+    
+}
